59cb7333c76ff55835f72f7a8edfe2d92fd63cbe
Katie Eagles <kteagles21@gmail.com>
2021-10-16 15:02:36 -0400

    fixed getDB and putDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ jate: content });
+  const request = store.put({ id: 1, jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
@@ -29,14 +29,14 @@ export const putDb = async (content) => {
 
 
 export const getDb = async () => {
-  console.log('Get all from database');
+  console.log('Get first from database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('jate', result.jate);
+  return result.jate;
 };
 
 
